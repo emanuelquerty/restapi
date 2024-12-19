9561e4bbca85f4125f4d954bd9bbda957f4b3f9e
Emanuel Inacio <emanuelinacio1@hotmail.com>
2024-12-19 04:17:42 -0700

    adds readme and changes cmd dir structure

---
+++ cmd/app/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"log"
+	"log/slog"
+	"os"
+	"restapi/app"
+	"restapi/middleware"
+	"restapi/storage/sqlite"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	Port string
+	DSN  string
+}
+
+func loadConfigFromEnv(config *Config) {
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("SERVER_PORT")
+	dsn := os.Getenv("DB_DSN")
+
+	config.Port = port
+	config.DSN = dsn
+}
+
+func main() {
+	config := &Config{}
+	loadConfigFromEnv(config)
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	db, err := sqlite.NewDB(config.DSN)
+	if err != nil {
+		logger.Error("could not open database", slog.String("error", err.Error()))
+		os.Exit(1)
+	}
+
+	app := app.New(logger, db)
+
+	app.Use(middleware.AccessLogger)
+	app.Use(middleware.SecurityHeaders)
+
+	err = app.ListenAndServe(config.Port)
+	if err != nil {
+		logger.Error("could not start server", slog.String("error", err.Error()))
+	}
+}
